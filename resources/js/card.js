56f37a05f188dc780d0bcc51f70e8a048c386fc7
palPalani <pal@targetbay.com>
2022-05-29 20:41:55 +0530

    Improved CS

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,3 +1,3 @@
 Nova.booting((Vue, router, store) => {
-    Vue.component('nova-list-card', require('./components/Card'))
-})
+    Vue.component('nova-list-card', require('./components/Card'));
+});
