472f106d2498507f0b4d4fbef8616e8942515ab7
palPalani <pal@targetbay.com>
2022-05-29 20:36:37 +0530

    wip - upgrading laravel nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,10 @@
 let mix = require('laravel-mix')
 
-mix.setPublicPath('dist').js('resources/js/card.js', 'js')
+//mix.setPublicPath('dist').js('resources/js/card.js', 'js')
+mix.setPublicPath('dist').js('resources/js/card.js', 'dist/js')
+    .vue({ version: 2 })
+    .webpackConfig({
+        externals: {
+            Vue: 'vue',
+        }
+    });
